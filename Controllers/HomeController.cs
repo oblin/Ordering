58b1fab198ed939eedfe23dee91395c4e56ac40a
Yiming <oblin@msn.com>
2017-01-09 17:26:52 +0800

    First Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Models;
+
+namespace Ordering.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var orders = new List<Order>()
+            {
+                new Order { Name = "Dones id elit non", LastUpdated = DateTime.Now.AddDays(-2), Description = "Test 1" },
+                new Order { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-5), Description = "Test 2" },
+                new Order { Name = "Maecenas sed", LastUpdated = DateTime.Now.AddDays(-7), Description = "Test 3" }
+            };
+
+            ViewBag.Orders = orders;
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ordering.Models
+{
+    public class Order
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
