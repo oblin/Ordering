ca987a513dcfd05d01b2f479a18a009767613139
Yiming <oblin@msn.com>
2017-01-10 11:09:40 +0800

    加入 NewClient Html Helper

---
+++ Models/Address.cs
@@ -0,0 +1,10 @@
+namespace Ordering.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Ordering.Models
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime? ContactDate { get; set; }
+        public string ClientType { get; set; }
+        public string Notes { get; set; }
+
+        public Address BillingAddress { get; set; }
+        public Address MailingAddress { get; set; }
+    }
+}
